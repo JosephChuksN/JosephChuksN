1380c00176fd61fe8d5fa8451e317d2b1ffb151e
Nwosu Joseph Chuks <chuksdj07@gmail.com>
2022-12-08 05:26:42 +0800

    added contents to projects page

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -11,14 +11,20 @@ const Projects:React.FC = () => {
     </span>
 <div className="w-full h-[50vh]  flex  ">
   <span className="flex w-full h-[80%]  lg:w-[38rem] lg:h-full rounded-md  bg-no-repeat bg-cover" style={{backgroundImage:`url(${linkpath})`}}></span>
-  <div className="flex gap-3 flex-col absolute w-[55%] -right-36  top-56 bg-gradient-to-l from-[#0c0f11] to-[#0d1114] py-3 px-16 shadow-xl shadow-[#00000082] rounded-md">
+  <div className="flex gap-3 flex-col absolute w-[55%] -right-36  top-48 bg-gradient-to-l from-[#0c0f11] to-[#0d1114] py-3 px-16 shadow-xl shadow-[#00000082] rounded-md">
     <span className="text-white text-xl font-headers">Linkpath</span>
     <span className="text-gray-300 font-para">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam rutrum diam leo, a 
       lacinia lacus condimentum id. Mauris sodales fringilla magna ut 
-      ullamcorper. Cras nec interdum dui. Duis ac urna vitae ipsum</span>
+      ullamcorper.Duis ac urna vitae ipsum</span>
+      <span className="text-gray-50 flex gap-5">
+        <span>React</span>
+        <span>Tailwind</span>
+        <span>Node.js</span>
+        <span>Express</span>
+      </span>
       <span className="text-white font-headers flex gap-5">
-        <span className="border-2 p-1">View Live</span>
-        <span className="border-2 p-1">Source Code</span>
+        <span className="border-2 p-1 rounded-md">View Live</span>
+        <span className="border-2 p-1 rounded-md">Source Code</span>
       </span>
   </div>
 </div>
