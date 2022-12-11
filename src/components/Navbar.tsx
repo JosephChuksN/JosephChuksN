754578a2d46c9d90e0ddf22f5871ba65005dd6ae
Nwosu Joseph Chuks <chuksdj07@gmail.com>
2022-12-11 09:26:09 +0800

    fixing navigation scroll position mobile

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,16 +15,16 @@ const Navbar:React.FC = () => {
       const navNode = navbar.current
          const currentScroll = window.pageYOffset
          if(currentScroll <= 0){
-          navNode?.classList.remove("shadow-lg", "backdrop-blur-lg", "md:py-4")
-          navNode?.classList.add("md:py-8")
+          navNode?.classList.remove("shadow-lg", "backdrop-blur-lg", "md:py-4", "fixed")
+          navNode?.classList.add("md:py-8", "sticky")
          }
          if(currentScroll > threshHold  && !navNode?.classList.contains("-translate-y-full")){
           navNode?.classList.remove("shadow-lg",  "backdrop-blur-lg")
           navNode?.classList.add("-translate-y-full")   
          }
          if(currentScroll < threshHold  && navNode?.classList.contains("-translate-y-full")){
-          navNode?.classList.remove("-translate-y-full", "md:py-8")
-          navNode?.classList.add("shadow-lg", "backdrop-blur-lg", "md:py-4" )
+          navNode?.classList.remove("-translate-y-full", "md:py-8", "sticky")
+          navNode?.classList.add("shadow-lg", "backdrop-blur-lg", "md:py-4", "fixed" )
           
          }
      
@@ -55,12 +55,12 @@ useEffect(() =>{
    
 
   return (
-    <div ref={navbar} className={`shadow-[#00000082] top-0 transition-all delay-75 duration-200 ease-in-out fixed w-full py-4 md:py-8 font-headers text-white z-50 `}>
+    <div ref={navbar} className={`shadow-[#00000082] box-border block top-0 transition-all delay-75 duration-200 ease-in-out fixed w-full py-4 md:py-8 font-headers text-white z-50 `}>
     <div className=" flex justify-between items-center px-3 lg:px-56 relative">
        <span className="text-2xl p-0.5 flex items-center justify-center font-bold text-[#F78D26] border-2 rounded-full border-[#F78D26] w-12 h-12  ">JN</span>
        <span onClick={()=>{setshow(!show)}} className="lg:hidden text-3xl p-1 text-[#F78D26]"><FontAwesomeIcon icon={faBars} /></span>
 
-       <div  className={`fixed overscroll-none lg:static py-36 lg:py-0 justify-start items-center lg:justify-between lg:items-start top-0  left-0  h-full w-full lg:w-1/3 flex flex-col md:flex-row bg-[#0d1114] transition-all duration-500 delay-100 ease-in-out z-10 lg:bg-transparent gap-10 lg:opacity-100  font-medium text-2xl lg:text-base   ${show? "opacity-100" : "opacity-0"}`}>
+       <div  className={`fixed flex box-border overscroll-none lg:static py-36 lg:py-0 justify-start items-center lg:justify-between lg:items-start top-0  left-0  h-full w-full lg:w-1/3  flex-col md:flex-row bg-[#0d1114] transition-all duration-500 delay-100 ease-in-out z-10 lg:bg-transparent gap-10 lg:opacity-100  font-medium text-2xl lg:text-base   ${show? "opacity-100" : "opacity-0"}`}>
         
             <span onClick={()=>{setshow(!show)}} className="absolute top-8 right-5 p-1 text-[#F78D26] text-4xl lg:hidden"><FontAwesomeIcon icon={faClose} /></span>
         
