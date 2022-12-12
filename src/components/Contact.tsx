e55cd0c14276158dc51c96b89fd16e2e809c38bf
Nwosu Joseph Chuks <chuksdj07@gmail.com>
2022-12-13 06:27:45 +0800

    added contact form

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -8,16 +8,19 @@ const Contact = () => {
     Contact 
     <span className="text-[#F78D26]">{`}`}</span>
     </span>
-    <div>
-        <span>
+    <div className="flex items-center justify-center">
+        <span className="text-gray-300 font-para w-2/5">
         Interested in working together? Reach out to me through the 
         contact form below. Feel free to reach out to me 
-        through my social media handles or send a mail
-        <a href="mailto:hellonwosu@gmail.com">hellonwosu@gmail.com</a>
+        through my social media handles or send a mail  <br />
+        <a className="text-[#F78D26] " href="mailto:hellonwosu@gmail.com">hellonwosu@gmail.com</a>
         </span>
         <div>
-            <form action="">
-
+            <form className="flex flex-col gap-5" action="">
+        <input type="text" name=""  />
+        <input type="text" name=""  />
+        <textarea name=""
+         />
             </form>
         </div>
     </div>
