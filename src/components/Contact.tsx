1a9e4713ad9fe4cb4a85c9c53f31d043e36517a1
Nwosu Joseph Chuks <chuksdj07@gmail.com>
2022-12-13 06:52:30 +0800

    styled contact form

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -8,18 +8,30 @@ const Contact = () => {
     Contact 
     <span className="text-[#F78D26]">{`}`}</span>
     </span>
-    <div className="flex items-center justify-center">
-        <span className="text-gray-300 font-para w-2/5">
+    <div className="flex flex-col lg:flex-row w-full  gap-10 content-start justify-center px-5 py-3">
+        <span className="text-gray-300 font-para  text-base w-[50%] p-2">
         Interested in working together? Reach out to me through the 
         contact form below. Feel free to reach out to me 
         through my social media handles or send a mail  <br />
         <a className="text-[#F78D26] " href="mailto:hellonwosu@gmail.com">hellonwosu@gmail.com</a>
         </span>
-        <div>
-            <form className="flex flex-col gap-5" action="">
-        <input type="text" name=""  />
-        <input type="text" name=""  />
-        <textarea name=""
+        <div className="w-[50%]">
+        <form className="flex flex-col gap-5 w-full p-2" action="">
+        <input className="rounded-sm p-2 bg-[#ffffff0f]" 
+         type="text" 
+         name=""  
+         placeholder="Name"
+         />
+        <input className="rounded-sm p-2 bg-[#ffffff0f]" 
+        type="email" 
+        name=""  
+        placeholder="Email"
+        />
+        <textarea className="rounded-sm p-2 bg-[#ffffff0f] resize-none"
+        name=""
+        cols={10}
+        rows={8}
+        placeholder="Your message"
          />
             </form>
         </div>
