1566fd450401502077b6f18d30b7a4bcf544a8d1
Nwosu Joseph Chuks <chuksdj07@gmail.com>
2022-12-15 05:51:04 +0800

    added hook to prevent scroll when navigation is open

---
+++ src/hooks/useNavbarControl.ts
@@ -0,0 +1,8 @@
+import { useState, useEffect } from "react";
+
+
+ const useNavbarControl = () :void => {
+    const [isNavOpen, setIsNavOpen] = useState<boolean>(false)
+}
+
+export default useNavbarControl
