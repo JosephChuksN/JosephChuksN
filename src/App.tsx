e5f198964b32831c9a23771ed286083d27391966
Nwosu Joseph Chuks <chuksdj07@gmail.com>
2022-12-27 20:28:17 +0800

    installed aos animation and implemented animation in project section

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
@@ -6,11 +6,18 @@ import About from './components/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import SocailNetworks from './components/SocailNetworks';
+import AOS from 'aos'
+import 'aos/dist/aos.css';
+
 
 
 
 const  App:React.FC = () => {
 
+
+  useEffect(()=>{
+    AOS.init({delay: 300, duration: 300, once: true, easing: "ease-in-out"})
+  },[])
 
   return (
     <div>
--- src/components/ProjectData.tsx
+++ src/components/ProjectData.tsx
@@ -1,6 +1,7 @@
 import React, {useState, useEffect, useRef} from 'react'
 import { ProjectInterface } from '../interfaces/projectDataInterface'
-import { Slide } from 'react-awesome-reveal'
+
+
 
 interface props {
     data:ProjectInterface
@@ -25,14 +26,11 @@ useEffect(()=>{
 
   return (
     <div key={data.name} className="w-full h-full  flex flex-col lg:flex-row items-center justify-center lg:pb-5 p-1 pb-16 ">
-      <div className="lg:w-[39rem] w-full h-full">
-      <Slide triggerOnce={true}  damping={0.3} direction="left">
-     <span ref={image} className={`flex  w-full h-[300px] lg:h-[65vh] rounded-l-md  bg-no-repeat bg-cover ${hover ? "scale-110": ""} `} style={{backgroundImage:`url(${data.image})`}}></span>
-     </Slide>
-      </div>
-     <div className="w-full lg:w-[30rem] " >
-     <Slide damping={0.5}  direction="right" triggerOnce={true}>
-     <div  className="flex gap-10  lg:gap-3 flex-col   h-full lg:h-[65vh]  justify-center   w-full  bg-[#000000b5]  lg:bg-gradient-to-l from-[#0c0f11] to-[#0d1114] py-3 px-2  lg:px-10  shadow-xl shadow-[#00000082] rounded-r-md">
+      
+     <span data-aos="zoom-in-down" ref={image} className={`flex  w-full h-[300px] lg:h-[65vh] rounded-l-md  bg-no-repeat bg-cover ${hover ? "scale-110": ""} `} style={{backgroundImage:`url(${data.image})`}}></span>
+    
+     
+     <div data-aos="zoom-out-up"  className="flex gap-10  lg:gap-3 flex-col   h-full lg:h-[65vh]  justify-center   w-full  bg-[#000000b5]  lg:bg-gradient-to-l from-[#0c0f11] to-[#0d1114] py-3 px-2  lg:px-10  shadow-xl shadow-[#00000082] rounded-r-md">
       <span className="text-[#F78D26] text-xl font-headers">{data.name}</span>
       <span className="text-gray-300 font-para">{data.description}</span>
         <span className="font-headers text-gray-50 flex gap-3 items-center  w-full">
@@ -45,8 +43,7 @@ useEffect(()=>{
           <a href={data.sourceCode} className="border-2 p-1 font-para rounded-md cursor-pointer text-base transition-all duration-300 delay-75 ease-in-out hover:text-[#F78D26] hover:border-[#F78D26] hover:scale-90"><span >Source Code</span></a>
         </span>
     </div>
-    </Slide>
-     </div>
+  
     
 </div>
 
