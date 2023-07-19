dd0ff2df099fec49fea2150adbae8d151590693a
Nwosu Joseph Chuks <chuksdj07@gmail.com>
2023-07-19 19:48:02 +0800

    fixes

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -7,11 +7,7 @@ const ContactPage:FC = () =>{
 
     return (
  <div className="flex flex-col  gap-10 md:w-3/5 mx-auto px-5 md:px-0  translate-y-10 pb-14">
-    <span className="text-4xl text-white font-semibold font-headers flex gap-1 mb-3 ">
-     <span className="text-[#F78D26]">{`{`}</span> 
-       Contact 
-     <span className="text-[#F78D26]">{`}`}</span>
-    </span>
+    <span className="text-4xl text-white font-semibold font-headers flex gap-1 mb-3 ">Contact </span>
   <div className="flex flex-col lg:flex-row w-full  gap-10 content-start justify-center lg:px-5 lg:py-3">
       <span className="text-gray-300 font-para  text-lg lg:w-[50%] lg:p-2">
       Interested in working together? Reach out to me through the 
